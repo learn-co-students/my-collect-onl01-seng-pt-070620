7c3dd9d041aeb24a10c70f000e1caec7a8736d7d
Justin Lupica <justinlupica@gmail.com>
2020-07-15 14:06:56 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,18 @@
+def my_collect(array)
+name = 0
+collection = []
+  while name < array.length
+  collection << yield(array[name])
+    yield(array[name])
+  name += 1
+    end
+array
+collection
+end
 
+my_collect(" ") do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
+end
 
