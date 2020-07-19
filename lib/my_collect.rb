19cef2c986533d08c6d1b3a2cb2f09368efecd8b
Billy Gunn, Jr <bgunnjr@gmail.com>
2020-07-19 04:23:39 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,20 @@
-
+def my_collect(array)
+  if array.size == 0
+    "Array is empty"
+  end
+  i = 0 
+  temp_array = []
+  while i < array.size
+    if array[i].include?(" ")
+      temp_array.push(yield(array[i].split(" ").first))
+      #execute code
+      i = i + 1
+    else 
+      temp_array.push(yield(array[i].upcase))
+      i = i + 1 
+    end
+      
+  end
+  temp_array
+end
 
